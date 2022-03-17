d88924ed03706d8953886f851563a68a1c6fbd5d
realizyy <realizy@student.telkomuniversity.ac.id>
2022-03-17 11:11:54 +0800

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using tpmodul4_1302204035;
-
-HaloGeneric halo = new HaloGeneric();
-
-//input name 
-Console.WriteLine("Masukkan nama user : ");
-string nama;
-halo.SetNama(nama = Console.ReadLine());
-//return nama
-Console.Clear();//Clear screen console
-Console.WriteLine("Halo user : " + halo.SapaUser());
-
-
 namespace tpmodul4_1302204035
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric halo = new HaloGeneric();
+
+        //input name 
+            Console.WriteLine("Masukkan nama user : ");
+            string nama;
+            halo.SetNama(nama = Console.ReadLine()); 
+        //return nama
+            Console.Clear();//Clear screen console
+            Console.WriteLine("Halo user : " + halo.SapaUser());
+            Console.ReadKey();
+
+        //DataGeneric
             DataGeneric<int> data1 = new DataGeneric<int>();
             data1.Add(1302204035);
             data1.PrintData();
