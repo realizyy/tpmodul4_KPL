cbb6bdd616621bd25017143e8ea6e91aa0e0e28b
realizyy <realizy@student.telkomuniversity.ac.id>
2022-03-17 10:30:23 +0800

    Versi 3, Menambahkan class DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,29 @@
+﻿namespace tpmodul4_1302204035;
+
+public class DataGeneric<T>
+{
+    private List<T> data;
+    public DataGeneric()
+    {
+        data = new List<T>();
+    }
+    
+    public void Add(T item)
+    {
+        data.Add(item);
+        System.Console.WriteLine("Data yang tersimpan adalah : " + item);
+    }
+
+    public void PrintData()
+    {
+        foreach (T item in data)
+        {
+            System.Console.WriteLine("Nim : " + item);
+        }
+    }
+    
+    /*public T PrintData(int index)
+    {
+        return data[index];
+    }*/
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+namespace tpmodul4_1302204035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<int> data1 = new DataGeneric<int>();
+            data1.Add(1302204035);
+            data1.PrintData();
+            //System.Console.WriteLine("Nim : " + data1.PrintData(0));
+        }
+    }
+}
